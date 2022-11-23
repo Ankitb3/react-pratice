1bc500547685ecf171f5c7002ab8b056013f9f59
ankit <bankarankit3@gmail.com>
2022-11-24 00:30:30 +0530

    added

---
+++ src/components/apple/Apple.js
@@ -0,0 +1,9 @@
+import './Apple.css'
+function Apple(){
+    return(
+        <>
+        <h2 className='apple'>Hello im apple</h2>
+        </>
+    )
+}
+export default Apple;
---
+++ src/components/mango/Mango.js
@@ -0,0 +1,9 @@
+import './Mango.css'
+function Mango() {
+    return (
+        <>
+            <h2 className='mango'>Hello im mango</h2>
+        </>
+    )
+}
+export default Mango;
